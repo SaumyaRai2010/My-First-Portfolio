b186956572f2a85235a1f418c4155cf643d41410
SaumyaRai2010 <saunya.rai2010@gmail.com>
2021-08-19 14:23:36 +0530

    19th August Afternoon

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,30 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect ,useRef} from "react";
 
 export default function Intro() {
+
+    const textRef= useRef();
+    useEffect(() => {
+        init(textRef.current, { showCursor: false, strings: ['Web Developer', 'Designer','Programmer','Fitness Expert'] })
+    }, []);
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man2.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hey There, I'm</h2>
+                    <h1>Saumya Rai</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png " alt="" />
+                </a>
+            </div>
         </div>
     )
 }
