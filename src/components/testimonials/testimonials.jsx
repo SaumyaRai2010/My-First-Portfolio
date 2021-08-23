760c67af39b4b9946179639beeba61d9f518b98e
SaumyaRai2010 <saunya.rai2010@gmail.com>
2021-08-23 14:38:15 +0530

    23 August 3PM

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -3,7 +3,7 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return (
         <div className='testimonials' id="testimonials">
-            
+            <h1>Testimonials</h1>
         </div>
     )
 }
