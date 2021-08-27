f74a7a1f22b855ce3fbfec4953eaa2f301d08ae2
SaumyaRai2010 <saunya.rai2010@gmail.com>
2021-08-27 23:37:30 +0530

    27th August 11:30PM

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,24 @@
 import "./contact.scss"
 
 export default function Contact() {
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+    }
     return (
         <div className='contact' id="contact">
-            
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Write Email" />
+                    <textarea name="" placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                    
+                </form>
+            </div>
         </div>
     )
 }
