28723086139378af5b78b69dadacabf48a10abfb
SaumyaRai2010 <saunya.rai2010@gmail.com>
2021-08-28 23:18:16 +0530

    28th August 11PM

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,5 +1,9 @@
 import "./contact.scss"
 import { useState } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 export default function Contact() {
 
